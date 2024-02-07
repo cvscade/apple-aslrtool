20ba62e9bd7ddc459ac81fa5c55cd4e439181258
cascade <52331239+cvscade@users.noreply.github.com>
2024-02-07 17:36:26 +0530

    chore: go back to Option<T>

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ use mach2::{
 use clap::Parser;
 use psutil::process;
 use std::{
-    collections::HashSet, error::Error, fmt, mem::size_of, ptr::addr_of_mut, sync::OnceLock,
+    collections::HashSet, mem::size_of, ptr::addr_of_mut, sync::OnceLock,
 };
 
 use libc::getuid;
@@ -28,14 +28,14 @@ struct ProcessDetails {
     pid: u32,
     #[arg(long, default_value_t = 0x100000000)]
     base_address: mach_vm_address_t,
-    #[arg(long)]
+    #[arg(long, default_value_t = String::from(""))]
     name: String,
 }
 
 const CS_RUNTIME: u32 = 0x10000;
+const RESET_COLOR: &str = "\x1b[0m";
 const RED_COLOR: &str = "\x1b[0;31m";
 const BLUE_COLOR: &str = "\x1b[0;34m";
-const RESET_COLOR: &str = "\x1b[0m";
 
 static BASE_ADDRESS: OnceLock<mach_vm_address_t> = OnceLock::new();
 
@@ -47,8 +47,8 @@ pub(crate) fn check_hardened_runtime(pid: u32) -> bool {
     (status & CS_RUNTIME) == CS_RUNTIME
 }
 
-fn fetch_target_pids(name: &str) -> Result<HashSet<u32>, Box<dyn Error>> {
-    let process_collector = process::ProcessCollector::new()?;
+fn fetch_target_pids(name: &str) -> Option<HashSet<u32>> {
+    let process_collector = process::ProcessCollector::new().unwrap();
 
     let process_list = process_collector.processes;
 
@@ -61,33 +61,7 @@ fn fetch_target_pids(name: &str) -> Result<HashSet<u32>, Box<dyn Error>> {
             pid_list.insert(pid);
         });
 
-    if !pid_list.is_empty() {
-        Ok(pid_list)
-    } else {
-        Err(Box::new(TaskFindError))
-    }
-}
-
-#[derive(Debug)]
-struct TaskFindError;
-
-impl Error for TaskFindError {}
-
-impl fmt::Display for TaskFindError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "[ {RED_COLOR}x{RESET_COLOR} ] Could not find task! Please check if hardened runtime is disabled for the running task.")
-    }
-}
-
-#[derive(Debug)]
-struct TaskGrabError;
-
-impl Error for TaskGrabError {}
-
-impl fmt::Display for TaskGrabError {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "[ {RED_COLOR}x{RESET_COLOR} ] Could not grab task!")
-    }
+    (!pid_list.is_empty()).then_some(pid_list)
 }
 
 fn task_to_slide(task: mach_port_t) -> Option<AslrSlide> {
@@ -113,24 +87,18 @@ fn task_to_slide(task: mach_port_t) -> Option<AslrSlide> {
         .then(|| (address - base_address) as AslrSlide)
 }
 
-fn pid_to_task(pid: u32) -> Result<mach_port_t, TaskGrabError> {
+fn pid_to_task(pid: u32) -> Option<mach_port_t> {
     let mut target_task: mach_port_t = 0;
 
-    if unsafe { task_for_pid(mach_task_self(), pid as i32, addr_of_mut!(target_task)) }
-        == KERN_SUCCESS
-    {
-        Ok(target_task)
-    } else {
-        Err(TaskGrabError)
-    }
+    (unsafe { task_for_pid(mach_task_self(), pid as i32, addr_of_mut!(target_task)) }
+        == KERN_SUCCESS)
+        .then_some(target_task)
 }
 
 fn main() {
-    if let Ok(args) = ProcessDetails::try_parse() {
-        if unsafe { getuid() } != 0 {
-            println!("[ {RED_COLOR}x{RESET_COLOR} ] Please run this program as root!");
-        }
-
+    if unsafe { getuid() } != 0 {
+        eprintln!("[ {RED_COLOR}x{RESET_COLOR} ] Please run this program as root!");
+    } else if let Ok(args) = ProcessDetails::try_parse() {
         if args.base_address != 0x100000000 {
             let _ = *BASE_ADDRESS.get_or_init(|| args.base_address);
         } else {
@@ -139,25 +107,32 @@ fn main() {
 
         if args.pid != 0 {
             let pid = args.pid;
-
-            let task = pid_to_task(pid).unwrap();
-
-            if let Some(slide) = task_to_slide(task) {
-                println!("[{BLUE_COLOR}PID{RESET_COLOR}: {pid}] ASLR Slide: {slide:#x}");
-            } else {
-                println!("[ {RED_COLOR}x{RESET_COLOR} ] Could not grab ASLR slide! Please check if the base address is correct.")
-            }
-        } else {
-            let pid_list = fetch_target_pids(args.name.as_str()).unwrap();
-
-            for pid in pid_list.iter().filter(|pid| !check_hardened_runtime(**pid)) {
-                let task = pid_to_task(*pid).unwrap();
-
+            if let Some(task) = pid_to_task(pid) {
                 if let Some(slide) = task_to_slide(task) {
                     println!("[{BLUE_COLOR}PID{RESET_COLOR}: {pid}] ASLR Slide: {slide:#x}");
                 } else {
-                    println!("[ {RED_COLOR}x{RESET_COLOR} ] Could not grab ASLR slide! Please check if the base address is correct.")
+                    eprintln!("[ {RED_COLOR}x{RESET_COLOR} ] Could not grab ASLR slide! Please check if the base address is correct.")
                 }
+            } else {
+                eprintln!("[ {RED_COLOR}x{RESET_COLOR} ] Could not grab task!");
+            }
+        } else {
+            if let Some(pid_list) = fetch_target_pids(args.name.as_str()) {
+                for pid in pid_list.iter().filter(|pid| !check_hardened_runtime(**pid)) {
+                    if let Some(task) = pid_to_task(*pid) {
+                        if let Some(slide) = task_to_slide(task) {
+                            println!(
+                                "[{BLUE_COLOR}PID{RESET_COLOR}: {pid}] ASLR Slide: {slide:#x}"
+                            );
+                        } else {
+                            eprintln!("[ {RED_COLOR}x{RESET_COLOR} ] Could not grab ASLR slide! Please check if the base address is correct.")
+                        }
+                    } else {
+                        eprintln!("[ {RED_COLOR}x{RESET_COLOR} ] Could not grab task!");
+                    }
+                }
+            } else {
+                eprintln!("[ {RED_COLOR}x{RESET_COLOR} ] Could not find task! Please check if hardened runtime is disabled for the running task.")
             }
         }
     } else {
